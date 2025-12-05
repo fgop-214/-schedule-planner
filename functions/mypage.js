f55a15d2f8c0ac6332783addab07b5bc5c983d21
fgop-214 <aoba@jamsquare.org>
2025-12-05 22:35:34 +0900

    Update mypage.js

--- functions/mypage.js
+++ functions/mypage.js
@@ -1,4 +1,4 @@
 export async function onRequestGet({ env }) {
   // middleware が先に発動する
-  return env.ASSETS.fetch("https://<your-domain>/mypage.html");
+  return env.ASSETS.fetch("https://schedule-planner-8dn.pages.dev/mypage.html");
 }
