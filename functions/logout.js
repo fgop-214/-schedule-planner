068522208d089062142b966dd3dc0b9791a6627c
fgop-214 <aoba@jamsquare.org>
2025-12-10 23:17:55 +0900

    Create logout.js

---
+++ functions/logout.js
@@ -0,0 +1,8 @@
+export async function onRequestGet() {
+  const headers = new Headers({
+    "Set-Cookie":
+      "user=; Path=/; HttpOnly; Secure; Max-Age=0; SameSite=Strict"
+  });
+
+  return Response.redirect("/index.html", { headers });
+}
